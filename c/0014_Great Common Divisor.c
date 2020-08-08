4e1ec624d3c8be06d4f4788d6d47cedb8396c8ea
Pongporamat Charuchinda <pongporamat.c@ku.th>
2020-08-08 18:22:04 +0700

    update c-0014

--- c/0014_Great Common Divisor.c
+++ c/0014_Great Common Divisor.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+int main(void)
+{
+    // My brain can't handle recursive function :(
+    unsigned int a, b, temp;
+    scanf("%u %u", &a, &b);
 
+    // I might come back for practicing the recursive function ;0
+    // if I have not forgotten ;)
+    // I think I surely will :(
+    while (1)
+    {
+        if (a>b)
+        {
+            temp = a;
+            a = b;
+            b = temp;
+        }
+        b = b%a;
+        if (b==0)
+            break;
+    }
+    printf("%u", a);
+    return 0;
+}
